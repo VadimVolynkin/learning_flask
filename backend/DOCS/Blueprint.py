7a8551268ad856b64539ed6aa20df941e01fb93a
Vadim Volynkin <71ooo@mail.ru>
2021-07-19 17:15:13 +0300

    finished blueprint adminpanel

--- backend/DOCS/Blueprint.py
+++ backend/DOCS/Blueprint.py
@@ -1,10 +1,9 @@
 
-
 # ============================================================================================================================
 # admin.py
 # ============================================================================================================================
 
-from flask import Blueprint
+from flask import Blueprint, session, redirect, url_for, request, render_template, g
 
 
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
@@ -13,11 +12,6 @@ admin = Blueprint('admin', __name__, template_folder='templates', static_folder=
 __name__         # имя исполняемого модуля, относительно которого будет искаться папка admin и соответствующие подкаталоги
 template_folder  # подкаталог для шаблонов этого Blueprint(по умолчанию берется подкаталог шаблонов приложения)
 static_folder    # подкаталог для статических файлов (по умолчанию берется подкаталог static приложения) 
-
-
-@admin.route('/')
-def index():
-    return "admin"
 
 # создание в сессии запись - админ авторизован
 def login_admin():
@@ -30,6 +24,112 @@ def isLogged():
 # выход из авторизации - стирает запись в сессии
 def logout_admin():
     session.pop('admin_logged', None)
+
+
+# обработчик логин
+# url_for('.index') указывает на index в текущем Blueprint
+# url_for('admin.index’) - это более конкретный вариант записи. Здесь admin – это имя Blueprint, а не название файла admin.py.
+@admin.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        if request.form['user'] == "admin" and request.form['psw'] == "12345":
+            login_admin()
+            return redirect(url_for('.index'))
+        else:
+            flash("Неверная пара логин/пароль", "error")
+ 
+    return render_template('admin/login.html', title='Админ-панель')
+
+
+# обработчик логаута
+@admin.route('/logout', methods=["POST", "GET"])
+def logout():
+    if not isLogged():
+        return redirect(url_for('.login'))
+ 
+    logout_admin()
+ 
+    return redirect(url_for('.login'))
+
+
+menu = [{'url': '.index', 'title': 'Панель'},
+        {'url': '.listusers', 'title': 'Список пользователей'},
+        {'url': '.listpubs', 'title': 'Список статей'},
+        {'url': '.logout', 'title': 'Выйти'}]
+
+
+# главная админ-панели
+@admin.route('/')
+def index():
+    if not isLogged():
+        return redirect(url_for('.login'))
+ 
+    return render_template('admin/index.html', menu=menu, title='Админ-панель')
+
+
+# вывод статей в панели администратора
+@admin.route('/list-pubs')
+def listpubs():
+    # если пользователь не авторизован - редирек на логинпейдж
+    if not isLogged():
+        return redirect(url_for('.login'))
+ 
+    # если есть соединение - получаем статьи из БД - рендерим
+    list = []
+    if db:
+        try:
+            cur = db.cursor()
+            cur.execute(f"SELECT title, text, url FROM posts")
+            list = cur.fetchall()
+        except sqlite3.Error as e:
+            print("Ошибка получения статей из БД " + str(e))
+ 
+    return render_template('admin/listpubs.html', title='Список статей', menu=menu, list=list)
+
+
+# вывод списка пользователей в админпанели 
+@admin.route('/list-users')
+def listusers():
+    if not isLogged():
+        return redirect(url_for('.login'))
+ 
+    list = []
+    if db:
+        try:
+            cur = db.cursor()
+            cur.execute(f"SELECT name, email FROM users ORDER BY time DESC")
+            list = cur.fetchall()
+        except sqlite3.Error as e:
+            print("Ошибка получения статей из БД " + str(e))
+ 
+    return render_template('admin/listusers.html', title='Список пользователей', menu=menu, list=list)
+
+
+# ============================================================================================================================
+# БД
+# ============================================================================================================================
+
+# установдение связи с БД - берем значение link_db из g-переменной контекста приложения
+db = None
+@admin.before_request
+def before_request():
+    """Установление соединения с БД перед выполнением запроса"""
+    global db
+    db = g.get('link_db')
+ 
+# закрытие соединения
+@admin.teardown_request
+def teardown_request(request):
+    global db
+    db = None
+    return request
+
+
+# декораторы уровня приложения как бы «обертывают» выполнение декораторов в Blueprint
+@app.before_request
+@admin.before_request
+@admin.teardown_request
+@app.teardown_appcontext 
 
 
 # ============================================================================================================================
@@ -46,34 +146,6 @@ url_prefix      # префикс для всех URL модуля admin. Без 
 
 http://127.0.0.1:5000/admin/ 
 домен/<url_prefix>/<URL-blueprint> 
-
-
-# АВТОРИЗАЦИЯ
-# url_for('.index') указывает на index в текущем Blueprint
-# url_for('admin.index’) - это более конкретный вариант записи. Здесь admin – это имя Blueprint, а не название файла admin.py.
-@admin.route('/login', methods=["POST", "GET"])
-def login():
-    if request.method == "POST":
-        if request.form['user'] == "admin" and request.form['psw'] == "12345":
-            login_admin()
-            return redirect(url_for('.index'))
-        else:
-            flash("Неверная пара логин/пароль", "error")
- 
-    return render_template('admin/login.html', title='Админ-панель')
-
-
-
-# ЛОГАУТ
-@admin.route('/logout', methods=["POST", "GET"])
-def logout():
-    if not isLogged():
-        return redirect(url_for('.login'))
- 
-    logout_admin()
- 
-    return redirect(url_for('.login'))
-
 
 
 # ============================================================================================================================
@@ -139,9 +211,12 @@ def logout():
 
 
 
+# ============================================================================================================================
+# templates/base.html
+# ============================================================================================================================
 
-
-
+# маршрут указывает на шаблон в blueprint
+<a href="{{ url_for('admin.index') }}">Админ-панель</a></p>
 
 
 
--- backend/admin/admin.py
+++ backend/admin/admin.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, session, redirect, url_for, request, render_template
+from flask import Blueprint, session, redirect, url_for, request, render_template, g
 
 
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
@@ -8,21 +8,6 @@ admin = Blueprint('admin', __name__, template_folder='templates', static_folder=
 # template_folder  # подкаталог для шаблонов этого Blueprint(по умолчанию берется подкаталог шаблонов приложения)
 # static_folder    # подкаталог для статических файлов (по умолчанию берется подкаталог static приложения) 
 
-
-# @admin.route('/')
-# def index():
-#     return "admin"
-
-@admin.route('/login', methods=["POST", "GET"])
-def login():
-    if request.method == "POST":
-        if request.form['user'] == "admin" and request.form['psw'] == "12345":
-            login_admin()
-            return redirect(url_for('.index'))
-        else:
-            flash("Неверная пара логин/пароль", "error")
- 
-    return render_template('admin/login.html', title='Админ-панель')
 
 # создание в сессии запись - админ авторизован
 def login_admin():
@@ -36,6 +21,20 @@ def isLogged():
 def logout_admin():
     session.pop('admin_logged', None)
 
+
+# обработчик логин
+@admin.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        if request.form['user'] == "admin" and request.form['psw'] == "12345":
+            login_admin()
+            return redirect(url_for('.index'))
+        else:
+            flash("Неверная пара логин/пароль", "error")
+ 
+    return render_template('admin/login.html', title='Админ-панель')
+
+
 # обработчик логаута
 @admin.route('/logout', methods=["POST", "GET"])
 def logout():
@@ -48,7 +47,10 @@ def logout():
 
 
 menu = [{'url': '.index', 'title': 'Панель'},
+        {'url': '.listusers', 'title': 'Список пользователей'},
+        {'url': '.listpubs', 'title': 'Список статей'},
         {'url': '.logout', 'title': 'Выйти'}]
+
 
 # главная админ-панели
 @admin.route('/')
@@ -59,6 +61,61 @@ def index():
     return render_template('admin/index.html', menu=menu, title='Админ-панель')
 
 
+# вывод статей в панели администратора
+@admin.route('/list-pubs')
+def listpubs():
+    # если пользователь не авторизован - редирек на логинпейдж
+    if not isLogged():
+        return redirect(url_for('.login'))
+ 
+    # если есть соединение - получаем статьи из БД - рендерим
+    list = []
+    if db:
+        try:
+            cur = db.cursor()
+            cur.execute(f"SELECT title, text, url FROM posts")
+            list = cur.fetchall()
+        except sqlite3.Error as e:
+            print("Ошибка получения статей из БД " + str(e))
+ 
+    return render_template('admin/listpubs.html', title='Список статей', menu=menu, list=list)
+
+
+# вывод списка пользователей в админпанели 
+@admin.route('/list-users')
+def listusers():
+    if not isLogged():
+        return redirect(url_for('.login'))
+ 
+    list = []
+    if db:
+        try:
+            cur = db.cursor()
+            cur.execute(f"SELECT name, email FROM users ORDER BY time DESC")
+            list = cur.fetchall()
+        except sqlite3.Error as e:
+            print("Ошибка получения статей из БД " + str(e))
+ 
+    return render_template('admin/listusers.html', title='Список пользователей', menu=menu, list=list)
 
 
 
+# ============================================================================================================================
+# БД
+# ============================================================================================================================
+
+# установдение связи с БД - берем значение link_db из g-переменной контекста приложения
+# мы берем соединение с БД, которое было установлено осноынм приложением
+db = None
+@admin.before_request
+def before_request():
+    """Установление соединения с БД перед выполнением запроса"""
+    global db
+    db = g.get('link_db')
+ 
+# закрытие соединения
+@admin.teardown_request
+def teardown_request(request):
+    global db
+    db = None
+    return request
